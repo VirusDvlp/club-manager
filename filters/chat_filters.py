ad86d325c2a976ec8fc877ed00066ae84de73599
Vladislav <vladislav@jarvis.localdomain>
2025-11-03 04:19:45 +0000

    Models bug

--- filters/chat_filters.py
+++ filters/chat_filters.py
@@ -1,7 +1,13 @@
 from aiogram import types
 
 
+from config import chat_settings
+
+
 class GroupFilter:
 
     def __call__(self, event: types.CallbackQuery | types.Message):
-        return event.chat.id == 1
+        if isinstance(event, types.CallbackQuery):
+            return event.message.chat.id == chat_settings.GROUP_ID
+        else:
+            return event.chat.id == chat_settings.GROUP_ID
