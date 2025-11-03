fce704badfb745d2b3c0bcdae2822fd45049acaf
Vladislav <vladislav@jarvis.localdomain>
2025-11-03 04:04:40 +0000

    Little bug user start handlers

--- handlers/user/__init__.py
+++ handlers/user/__init__.py
@@ -5,6 +5,8 @@ from .start import register_user_start_handlers
 
 
 def register_user_handlers(dp):
+    register_user_start_handlers(dp)
+
     register_event_membership_handlers(dp)
 
     register_account_handlers(dp)
