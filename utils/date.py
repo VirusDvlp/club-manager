cfddd92c2d22c300a2b79094098decb97cf87ee0
Vladislav <vladislav@jarvis.localdomain>
2025-11-03 04:53:06 +0000

    Bugs in utils/date.py

--- utils/date.py
+++ utils/date.py
@@ -7,11 +7,11 @@ def validate_date_time(prompt: str) -> datetime:
    # (DD-MM-YYYY hh:mm)
     regex = r"^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\d{4} (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$"
 
-    if not re.match(regex, date_string):
+    if not re.match(regex, prompt):
         return False
 
     try:
-        res = datetime.strptime(date_string, "%d-%m-%Y %H:%M")
+        res = datetime.strptime(prompt, "%d-%m-%Y %H:%M")
     except ValueError:
         return False
 
